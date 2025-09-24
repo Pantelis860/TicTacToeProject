71828eae19edbfdaf4d161b64d86ddf5989e3ec4
Pantelis860 <iis22097@uom.edu.gr>
2025-09-25 00:52:32 +0300

    Initial commit

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,27 @@
+export default function GameBoard({onSelectSquare , board}) {
+
+   /* const [gameBoard, setGameBoard] = useState(initalGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+            return updatedGameBoard;
+        });
+
+        onSelectSquare();
+    } */
+
+    return <ol id="game-board">
+        {board.map((row, rowIndex) => <li key={rowIndex}>
+            <ol>
+                {row.map((playerSymbol, colIndex) => 
+                    <li key={colIndex} >
+                        <button onClick = {() => onSelectSquare(rowIndex, colIndex)} disabled = {playerSymbol !== null}>
+                            {playerSymbol}
+                        </button>
+                    </li>)}
+            </ol>
+        </li>)}
+    </ol>
+}
